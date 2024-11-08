02f4cfad631e8005c662f33d974470cf12918302
Raunak Kumar <71320898+raunakmishraa@users.noreply.github.com>
2024-11-08 23:23:30 +0545

    Update functions.py

--- tickets/functions.py
+++ tickets/functions.py
@@ -4,7 +4,7 @@ from email.message import EmailMessage
 
 def send_issue_status_email(user,ticketNo,status,raised_date):
     email_id = 'codersbuzz9@gmail.com'
-    pswd = creaedoeycydpbnm
+    pswd = 'creaedoeycydpbnm'
     email = EmailMessage()
     email['Subject'] = f'Issue #{ticketNo} {status} !!'
     email['From'] = email_id
