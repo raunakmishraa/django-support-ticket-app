d89367e9db8b970cdd9adea237806c9d64568d7c
Raunak Kumar <71320898+raunakmishraa@users.noreply.github.com>
2024-11-11 21:14:09 +0545

    Update settings.py

--- support/settings.py
+++ support/settings.py
@@ -121,7 +121,7 @@ LOGOUT_REDIRECT_URL = '/'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = './tickets/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
